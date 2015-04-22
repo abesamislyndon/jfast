aed8846707a6dd12c82c4b393edcb0cbb6e237d5
dondon <abesamislyndon@gmail.com>
2015-04-22 09:46:34 -0700

    fix  routing for login for admin and normal user

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,26 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+
+  def authenticate_admin_user!
+  authenticate_user!
+    unless current_user.admin?
+    flash[:alert] = "This area is restricted to administrators only."
+    redirect_to root_path
+  end
+ end
+
+def current_admin_user
+  return nil if user_signed_in? && !current_user.admin?
+  current_user
+end
+
+def after_sign_in_path_for(resource)
+if current_user.admin?
+	authenticated_root_path
+else
+    login_index_path
+end
+end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+devise_for :users
+
+ devise_scope :user do
+  authenticated :user do
+    root 'dashboard#index', as: :authenticated_root
+  end
+
+
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end
+end
+
+get 'login/index'
+
+end
